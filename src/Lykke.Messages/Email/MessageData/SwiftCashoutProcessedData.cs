a13127a7100f22ffeb24ea92346d8a7021b1914b
Oleksandr Lysenko <oleksandr.lysenko@pl.pwc.com>
2017-08-17 11:40:47 +0200

    #LWDEV-2708 update Email.MessageData

--- src/Lykke.Messages/Email/MessageData/SwiftCashoutProcessedData.cs
+++ src/Lykke.Messages/Email/MessageData/SwiftCashoutProcessedData.cs
@@ -3,15 +3,8 @@
     public class SwiftCashoutProcessedData : IEmailMessageData
     {
         public const string QueueName = "SwiftCashoutProcessed";
-
-        public string AssetId { get; set; }
-        public double Amount { get; set; }
+        
         public string FullName { get; set; }
-        public string Bic { get; set; }
-        public string AccNum { get; set; }
-        public string AccName { get; set; }
-        public string BankName { get; set; }
-        public string AccHolderAddress { get; set; }
         public string Year { get; set; }
     }
 }
