cded002e19c620523641679609a344cd13b9b8b8
Mikhail Musin <mikhail.musin@gmail.com>
2017-08-29 01:01:40 +0300

    Password recovery data

---
+++ src/Lykke.Messages/Email/MessageData/FreezePeriodNotificationData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lykke.Messages.Email.MessageData
+{
+    public class FreezePeriodNotificationData : IEmailMessageData
+    {
+        public DateTime FreezePeriod { get; set; }
+        public string Year { get; set; }
+
+        public const string QueueName = "FreezePeriodNotificationEmail";        
+    }
+}
---
+++ src/Lykke.Messages/Email/MessageData/PasswordRecoveryEmailComfirmationData.cs
@@ -0,0 +1,10 @@
+﻿namespace Lykke.Messages.Email.MessageData
+{    
+    public class PasswordRecoveryEmailComfirmationData : IEmailMessageData
+    {
+        public string ConfirmationCode { get; set; }
+        public string Year { get; set; }
+
+        public const string QueueName = "PasswordRecoveryConfirmationEmail";        
+    }    
+}
