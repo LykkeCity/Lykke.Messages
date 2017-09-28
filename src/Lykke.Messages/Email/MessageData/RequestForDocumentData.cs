e1be7a49882fe5f90717874fccaf28c9bcd162f3
Oleksandr Lysenko <monarch.lysenko@gmail.com>
2017-09-28 19:54:23 +0200

    Update RequestForDocumentData

--- src/Lykke.Messages/Email/MessageData/RequestForDocumentData.cs
+++ src/Lykke.Messages/Email/MessageData/RequestForDocumentData.cs
@@ -4,10 +4,10 @@
     {
         public const string QueueName = "RequestForDocument";
 
-        public string ClientId { get; set; }
+
+        public string FullName { get; set; }
         public string Text { get; set; }
         public string Comment { get; set; }
-        public string Amount { get; set; }
-        public string AssetId { get; set; }
+        public string Year { get; set; }
     }
 }
