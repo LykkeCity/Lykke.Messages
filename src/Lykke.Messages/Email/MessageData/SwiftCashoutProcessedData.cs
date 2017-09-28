14eb3c6b7e864c66675e7bdccabe14f7e8b9c9e5
Oleksandr Lysenko <monarch.lysenko@gmail.com>
2017-09-28 20:06:15 +0200

    update SwiftCashoutProcessedData

--- src/Lykke.Messages/Email/MessageData/SwiftCashoutProcessedData.cs
+++ src/Lykke.Messages/Email/MessageData/SwiftCashoutProcessedData.cs
@@ -4,13 +4,7 @@
     {
         public const string QueueName = "SwiftCashoutProcessed";
 
-        public string AssetId { get; set; }
-        public double Amount { get; set; }
         public string FullName { get; set; }
-        public string Bic { get; set; }
-        public string AccNum { get; set; }
-        public string AccName { get; set; }
-        public string BankName { get; set; }
-        public string AccHolderAddress { get; set; }
+        public string Year { get; set; }
     }
 }
