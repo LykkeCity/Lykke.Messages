dd034bd86361bd2810e16791097ef74a161b8895
Yury Batsyuro <yury@batsyuro.ru>
2017-09-28 16:35:38 +0300

    Adjusted models to fit new requirements

--- src/Lykke.Messages/Email/MessageData/RequestForDocumentData.cs
+++ src/Lykke.Messages/Email/MessageData/RequestForDocumentData.cs
@@ -3,10 +3,11 @@
     public class RequestForDocumentData : IEmailMessageData
     {
         public const string QueueName = "RequestForDocument";
-        
-        public string FullName { get; set; }
+
+        public string ClientId { get; set; }
         public string Text { get; set; }
         public string Comment { get; set; }
-        public string Year { get; set; }
+        public string Amount { get; set; }
+        public string AssetId { get; set; }
     }
 }
