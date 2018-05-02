87922b0f9d9a2d97d05fe3593f1702c52f03bfee
Dmitry Samodov <samodov.dmitry@gmail.com>
2018-05-02 15:03:35 +0300

    no account on password recovery data added

---
+++ src/Lykke.Messages/Email/MessageData/NoAccountPasswordRecovery.cs
@@ -0,0 +1,9 @@
+﻿namespace Lykke.Messages.Email.MessageData
+{
+    public class NoAccountPasswordRecoveryData : IEmailMessageData
+    {
+        public const string QueueName = "NoAccountPasswordRecoveryEmail";
+
+        public string Email { get; set; }
+    }
+}
